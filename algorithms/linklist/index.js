f52ec9458aaf440207bf373b34883c284093ec7e
liyan.ly18 <liyan.ly18@gmail.com>
2019-09-03 10:32:13 +0800

    Update lru

---
+++ algorithms/linklist/index.js
@@ -0,0 +1,251 @@
+function LinkNode(value) {
+    this.value = value;
+    this.prev = null;
+    this.next = null;
+}
+
+function LinkList() {
+    this.head = null;
+    this.tail = null;
+
+    this.length = 0;
+}
+
+LinkList.prototype.append = function (value) {
+    let node = new LinkNode(value);
+
+    if (this.length === 0) {
+        this.head = node;
+        this.tail = node;
+    } else {
+        node.prev = this.tail;
+        this.tail.next = node;
+        this.tail = node;
+    }
+
+    this.length++;
+
+    return node;
+};
+
+LinkList.prototype.appendAt = function (pos, value) {
+    if (pos < 0 || pos > this.length) {
+        return null;
+    }
+
+    let current = this.head;
+    let currentIndex = 0;
+    let node = new LinkNode(value);
+
+    if (pos === 0) {
+        current.prev = node;
+        node.next = current;
+        this.head = node;
+    } else {
+        while (currentIndex++ !== pos) {
+            current = current.next;
+        }
+
+        node.prev = current.prev;
+        current.prev.next = node;
+
+        node.next = current;
+        current.prev = node;
+    }
+
+    this.length++;
+
+    return node;
+};
+
+LinkList.prototype.remove = function (value) {
+    let current = this.head;
+
+    while (current) {
+        if (current.value === value) {
+            if (current === this.head && current === this.tail) {
+                this.head = null;
+                this.tail = null;
+            } else if (current === this.head) {
+                this.head = current.next;
+                this.head.prev = null;
+            } else if (current === this.tail) {
+                this.tail = current.prev;
+                this.tail.next = null;
+            } else {
+                current.prev.next = current.next;
+                current.next.prev = current.prev;
+
+                current.prev = null;
+                current.next = null;
+            }
+            this.length--;
+            return current;
+        }
+        current = current.next;
+    }
+    return null;
+
+};
+
+LinkList.prototype.removeAt = function (pos) {
+    if (pos < 0 || pos > this.length) {
+        return null;
+    }
+
+    let current = this.head;
+    let index = 0;
+
+    while (index++ !== pos) {
+        current = current.next;
+    }
+
+    if (current === this.head && current === this.tail) {
+        this.head = null;
+        this.tail = null;
+    } else if (current === this.head) {
+        this.head = current.next;
+        this.head.prev = null;
+    } else if (current === this.tail) {
+        this.tail = current.prev;
+        this.tail.next = null;
+    } else {
+        current.prev.next = current.next;
+        current.next.prev = current.prev;
+
+        current.prev = null;
+        current.next = null;
+    }
+
+    this.length--;
+
+    return current;
+};
+
+LinkList.prototype.find = function (value) {
+    let current = this.head;
+
+    while (current) {
+        if (current.value === value) {
+            return current;
+        }
+        current = current.next;
+    }
+
+    return null;
+};
+
+LinkList.prototype.move = function (value, markValue) {
+    if (value === markValue) {
+        return null;
+    }
+
+    let node = this.find(value), markNode = this.find(markValue);
+
+    if (node === null || markNode === null) {
+        return null;
+    }
+
+    if (node === this.head) {
+        this.head = node.next;
+        node.next.prev = null;
+    } else if (node === this.tail) {
+        this.tail = node.prev;
+        node.prev.next = null;
+    } else {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    if (markNode === this.tail) {
+        this.tail = node;
+        node.next = null;
+
+        markNode.next = node;
+        node.prev = markNode;
+    } else {
+        node.next = markNode.next;
+        markNode.next.prev = node;
+
+        markNode.next = node;
+        node.prev = markNode;
+    }
+};
+
+LinkList.prototype.reverse = function () {
+    let current = this.head;
+
+    while (current) {
+        let next = current.next;
+
+        current.next = current.prev;
+        current.prev = next;
+
+        current = next;
+    }
+
+    let tail = this.tail;
+    this.tail = this.head;
+    this.head = tail;
+};
+
+LinkList.prototype.traverse = function (fn) {
+    let current = this.head;
+    let index = 0;
+
+    while (current) {
+        fn.call(this, current.value, index, current);
+        current = current.next;
+        index++;
+    }
+};
+
+LinkList.prototype.print = function () {
+    this.traverse((value, index) => {
+        console.log(`pos:${index}, value:${value}`);
+    })
+};
+
+(function () {
+    const linkList = new LinkList();
+    linkList.append(1);
+    linkList.append(2);
+    linkList.append(3);
+    console.log('1-2-3');
+    linkList.print();
+
+    linkList.appendAt(1, 11);
+    console.log('1-11-2-3');
+    linkList.print();
+
+    linkList.remove(11);
+    console.log('1-2-3');
+    linkList.print();
+
+    linkList.removeAt(1);
+    console.log('1-3');
+    linkList.print();
+
+    console.log('linkNode value is 3');
+    console.log(linkList.find(3));
+
+    linkList.appendAt(1, 2);
+    console.log('1-2-3');
+    linkList.print();
+
+    linkList.move(1, 3)
+    console.log('2-3-1');
+    linkList.print();
+
+    linkList.move(1, 2)
+    console.log('2-1-3');
+    linkList.print();
+
+    linkList.move(2, 1)
+    console.log('1-2-3');
+    linkList.print();
+
+    linkList.reverse();
+    console.log('3-2-1');
+    linkList.print();
+})();
--- algorithms/lru/index.js
+++ algorithms/lru/index.js
@@ -1,8 +1,23 @@
+function LinkNode(value, key) {
+    this.value = value;
+    this.key = key;
+    this.prev = null;
+    this.next = null;
+}
+
 /**
  * @param {number} capacity
  */
 var LRUCache = function (capacity) {
-    
+    this.capacity = capacity;
+    this.size = 0;
+    this.cache = {};
+
+    this.head = new LinkNode();
+    this.tail = new LinkNode();
+
+    this.head.next = this.tail;
+    this.tail.prev = this.head;
 };
 
 /** 
@@ -10,7 +25,15 @@ var LRUCache = function (capacity) {
  * @return {number}
  */
 LRUCache.prototype.get = function (key) {
+    let node = this.cache[key];
 
+    if (!node) {
+        return -1;
+    }
+
+    this.moveToHead(node);
+
+    return node.value;
 };
 
 /** 
@@ -19,7 +42,67 @@ LRUCache.prototype.get = function (key) {
  * @return {void}
  */
 LRUCache.prototype.put = function (key, value) {
+    let node = this.cache[key];
 
+    if (node) {
+        node.value = value;
+        this.moveToHead(node);
+    } else {
+        node = new LinkNode(value, key);
+        this.cache[key] = node;
+        this.addNode(node);
+
+        if (++this.size > this.capacity) {
+            let tail = this.popTail();
+            delete this.cache[tail.key];
+
+            --this.size;
+        }
+    }
+};
+
+LRUCache.prototype.addNode = function (node) {
+    node.prev = this.head;
+    node.next = this.head.next;
+
+    this.head.next.prev = node;
+    this.head.next = node;
+};
+
+LRUCache.prototype.moveToHead = function (node) {
+    this.remove(node);
+
+    this.addNode(node);
+};
+
+LRUCache.prototype.popTail = function () {
+    let node = this.tail.prev;
+
+    this.remove(node);
+    return node;
+};
+
+LRUCache.prototype.remove = function (node) {
+    let prev = node.prev, next = node.next;
+
+    prev.next = next;
+    next.prev = prev;
+
+    node.prev = null;
+    node.next = null;
+};
+
+LRUCache.prototype.show = function (prefix) {
+    let node = this.head.next;
+
+    console.log(prefix);
+    while (node) {
+        if (node === this.tail) {
+            return;
+        }
+        console.log(node.value);
+        node = node.next;
+    }
 };
 
 /** 
@@ -33,12 +116,21 @@ LRUCache.prototype.put = function (key, value) {
     const cache = new LRUCache(2 /* 缓存容量 */);
 
     cache.put(1, 1);
+    cache.show('[LRU] 1');
     cache.put(2, 2);
+    cache.show('[LRU] 2-1');
     cache.get(1);       // 返回  1
+    cache.show('[LRU] 1-2');
     cache.put(3, 3);    // 该操作会使得密钥 2 作废
+    cache.show('[LRU] 3-1');
     cache.get(2);       // 返回 -1 (未找到)
+    cache.show('[LRU] 3-1');
     cache.put(4, 4);    // 该操作会使得密钥 1 作废
+    cache.show('[LRU] 4-3');
     cache.get(1);       // 返回 -1 (未找到)
+    cache.show('[LRU] 4-3');
     cache.get(3);       // 返回  3
+    cache.show('[LRU] 3-4');
     cache.get(4);       // 返回  4。
+    cache.show('[LRU] 4-3');
 })()
